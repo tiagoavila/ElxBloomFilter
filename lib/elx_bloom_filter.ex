2d23346edc684cd071163f170e9ba0428e24f0ab
Tiago Avila <tiago.avila09@gmail.com>
2023-07-19 18:51:18 -0300

    Implemented new and add methods

--- lib/elx_bloom_filter.ex
+++ lib/elx_bloom_filter.ex
@@ -3,28 +3,50 @@ defmodule ElxBloomFilter do
   Documentation for `ElxBloomFilter`.
   """
 
-  @doc """
-  Hello world.
+  @multiplier 10
 
-  ## Examples
+  # Seed values for hash functions
+  @seed {9_881_409, 7_740_287, 1_822_091, 6_436_985, 6_108_165, 9_294_15, 1_815_555, 1_670_246, 7_190_510, 1_923_245}
 
-      iex> ElxBloomFilter.hello()
-      :world
+  # The number of hash functions to use
+  @hashfunctions_count 6
 
-  """
-  def hello do
-    :world
+  defmodule Bloom do
+    defstruct bit_array: <<>>, capacity: 0, bit_array_length: 0
   end
 
-  def new() do
+  @spec new(pos_integer) :: any
+  def new(capacity) when is_integer(capacity) and capacity > 0 do
+    bit_array_length = capacity * @multiplier
+
+    %Bloom{bit_array: <<0::size(bit_array_length)>>, capacity: capacity, bit_array_length: bit_array_length}
+  end
+
+  def add(bloom_filter, value) do
+    updated_bit_array = 1..@hashfunctions_count
+    |> Enum.reduce(bloom_filter.bit_array, fn i, bit_array ->
+      index =
+        value
+        |> Murmur.hash_x86_32(elem(@seed, i))
+        |> rem(bloom_filter.bit_array_length)
+
+      set_bit_in_array(bit_array, index)
+    end)
+
+    %{bloom_filter | bit_array: updated_bit_array} |> IO.inspect()
+  end
+
+  def has?(bloom_filter, value) do
 
   end
 
-  def add() do
-
-  end
-
-  def member?() do
-
+  defp set_bit_in_array(bit_array, index) do
+    <<prefix::size(index), val::size(1), rest::bits>> = bit_array
+    # set the bit only if its not set
+    if val != 1 do
+      <<prefix::size(index), 1::size(1), rest::bits>>
+    else
+      bit_array
+    end
   end
 end
--- test/elx_bloom_filter_test.exs
+++ test/elx_bloom_filter_test.exs
@@ -2,7 +2,13 @@ defmodule ElxBloomFilterTest do
   use ExUnit.Case
   doctest ElxBloomFilter
 
-  test "greets the world" do
-    assert ElxBloomFilter.hello() == :world
+  test "create a new Bloom Filter" do
+    assert ElxBloomFilter.new(10) != nil
+  end
+
+  test "adding element to Bloom Filter" do
+    bloom_filter = ElxBloomFilter.new(10)
+    bloom_filter = ElxBloomFilter.add(bloom_filter, 5)
+    assert bloom_filter != nil
   end
 end
