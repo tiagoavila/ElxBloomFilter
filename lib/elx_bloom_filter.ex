06067c96a0ec8fb500a2ca00781951e2852d15bd
Tiago Avila <tiago.avila09@gmail.com>
2023-07-19 12:48:50 -0300

    Initial project

---
+++ lib/elx_bloom_filter.ex
@@ -0,0 +1,30 @@
+defmodule ElxBloomFilter do
+  @moduledoc """
+  Documentation for `ElxBloomFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElxBloomFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def new() do
+
+  end
+
+  def add() do
+
+  end
+
+  def member?() do
+
+  end
+end
---
+++ test/elx_bloom_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule ElxBloomFilterTest do
+  use ExUnit.Case
+  doctest ElxBloomFilter
+
+  test "greets the world" do
+    assert ElxBloomFilter.hello() == :world
+  end
+end
