57ac5d44baa77160c00f6a56b60a94b2d1d29a26
Tiago Avila <tiago.avila09@gmail.com>
2023-07-20 09:47:17 -0300

    added more test cases

--- test/elx_bloom_filter_test.exs
+++ test/elx_bloom_filter_test.exs
@@ -13,6 +13,38 @@ defmodule ElxBloomFilterTest do
     bloom_filter = ElxBloomFilter.add(bloom_filter, 5)
     assert ElxBloomFilter.has?(bloom_filter, 5) == true
 
-    assert ElxBloomFilter.has?(bloom_filter, 6) == false
+    assert ElxBloomFilter.has?(bloom_filter, "missing") == false
+  end
+
+  test "adding many items and checking all should return true" do
+    capacity = 100
+    items = 1..capacity |> Enum.map(fn _ -> :rand.uniform(1_000) end)
+
+    bloom_filter =
+      items
+      |> Enum.reduce(ElxBloomFilter.new(capacity), &ElxBloomFilter.add(&2, &1))
+
+    has_all_items =
+      items
+      |> Enum.all?(&ElxBloomFilter.has?(bloom_filter, &1))
+
+    assert has_all_items == true
+  end
+
+  test "adding many items, change one item value and checking all should return false" do
+    capacity = 100
+    items = 1..capacity |> Enum.map(fn _ -> :rand.uniform(1_000) end)
+
+    bloom_filter =
+      items
+      |> Enum.reduce(ElxBloomFilter.new(capacity), &ElxBloomFilter.add(&2, &1))
+
+    items = [1001 | items]
+
+    has_all_items =
+      items
+      |> Enum.all?(&ElxBloomFilter.has?(bloom_filter, &1))
+
+    assert has_all_items == false
   end
 end
